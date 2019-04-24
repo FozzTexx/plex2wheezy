f969d04cb93cfc7cd2e08af9380886ee7d263a4b
Chris Osborn <fozztexx@fozztexx.com>
2019-04-24 09:46:12 -0700

    Don't build a new package if there isn't one.

--- package.sh
+++ package.sh
@@ -3,11 +3,17 @@ URL="https://plex.tv/downloads/details/1?build=linux-ubuntu-x86_64&channel=16&di
 TEMP=tmp.$$
 
 DIRECT=$(curl -s "${URL}" | grep -Po '(?<=url=\")(\S+)(?=\")')
-echo ${DIRECT}
+PKG=$(basename "${DIRECT}")
+VERSION=$(echo "${PKG}" | sed -e 's/plexmediaserver_// ; s/_amd64.*/-debian/')
+DEB=plexmediaserver_"${VERSION}"-sysvinit_amd64.deb
+if [ -e "${DEB}" ] ; then
+    No new version
+    ls -Fl ${DEB}
+    exit
+fi
 mkdir ${TEMP}
 cd ${TEMP}
 wget -N "${DIRECT}"
-PKG=$(basename "${DIRECT}")
 mkdir dist
 dpkg -x "${PKG}" dist
 mkdir -p wheezy/etc/default wheezy/etc/init.d
@@ -15,10 +21,9 @@ sed -e 's/\(# *\)\?export //' dist/etc/default/plexmediaserver > wheezy/etc/defa
 cp ../init.d-plexmediaserver wheezy/etc/init.d/plexmediaserver
 chmod +x wheezy/etc/init.d/plexmediaserver
 rsync -a ../DEBIAN wheezy/.
-VERSION=$(echo "${PKG}" | sed -e 's/plexmediaserver_// ; s/_amd64.*/-debian/')
 sed -i -e 's/^Version:.*/Version: '"${VERSION}"/ wheezy/DEBIAN/control
 mv dist/usr wheezy/.
 dpkg-deb --build wheezy
-mv wheezy.deb ../plexmediaserver_"${VERSION}"-sysvinit_amd64.deb
+mv wheezy.deb ../${DEB}
 cd ..
 rm -rf ${TEMP}
